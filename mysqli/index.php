919e69f985a3bdcd8d772b1237bc0c16cf05a015
ngo <ngoyarzo@gmail.com>
2023-10-11 16:53:09 -0300

    first commit

--- mysqli/index.php
+++ mysqli/index.php
@@ -1,1 +1,15 @@
+<?php
+
+$mysqli = mysqli_connect("example.com", "user", "password", "database");
+
+$result = mysqli_query($mysqli, "SELECT 'A world full of ' AS _msg FROM DUAL");
+$row = mysqli_fetch_assoc($result);
+echo $row['_msg'];
+
+$mysqli = new mysqli("example.com", "user", "password", "database");
+
+$result = $mysqli->query("SELECT 'choices to please everybody.' AS _msg FROM DUAL");
+$row = $result->fetch_assoc();
+echo $row['_msg'];
+?>
 Hola mysqli
