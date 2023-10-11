ba8ef0dc9b5301a5ca68080a7ed0425825a49721
ngo <ngoyarzo@gmail.com>
2023-10-11 16:50:29 -0300

    first commit

--- mysqli/index.php
+++ mysqli/index.php
@@ -1,11 +1,1 @@
-<?php
-
-$mysqli = new mysqli("svc-mycluster", "root", "nolaseyo.", "mysql");
-
-echo $mysqli->host_info . "\n";
-
-$mysqli = new mysqli("mycluster.ddbb.svc.cluster.local", "root", "nolaseyo.", "mysql", 3306);
-
-echo $mysqli->host_info . "\n";
-?>
 Hola mysqli
