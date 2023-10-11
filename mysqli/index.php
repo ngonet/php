8c992cea3f3c1acebc7edeed652da02f2c8aef31
ngo <ngoyarzo@gmail.com>
2023-10-11 19:51:58 -0300

    first commit

--- mysqli/index.php
+++ mysqli/index.php
@@ -1,9 +1,9 @@
 <?php 
-      echo "<pre>";
-      print_r(get_loaded_extensions());
-      echo "<pre/>";
-?>
-<?php
+
+echo "<pre>";
+print_r(get_loaded_extensions());
+echo "<pre/>";
+
 $servername = "mycluster";
 $username = "root";
 $password = "nolaseyo.";
@@ -16,5 +16,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+mysqli_debug("d:t:o,/tmp/client.trace");
+
 ?>
 Hola mysqli
