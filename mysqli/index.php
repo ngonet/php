35cedd79dadb9cb3b8c5b93055e2c82b1a0e2c9e
ngo <ngoyarzo@gmail.com>
2023-10-11 18:48:07 -0300

    first commit

--- mysqli/index.php
+++ mysqli/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = mysqli_connect("svc-mycluster", "root", "nolaseyo.", "mysql");
+$mysqli = mysqli_connect("mycluster", "root", "nolaseyo.", "mysql");
 
 $result = mysqli_query($mysqli, "SELECT 'A world full of ' AS _msg FROM DUAL");
 $row = mysqli_fetch_assoc($result);
